5473436b08921a319431a5491e9578eecd98a388
Harman-singh-waraich <Waraichharman68@gmail.com>
2024-06-25 16:56:13 +0530

    fix(web): fix-svg-width-on-mobile

--- web/src/components/PreviewCard/Terms/AttachedFile.tsx
+++ web/src/components/PreviewCard/Terms/AttachedFile.tsx
@@ -1,23 +1,18 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import styled, { css } from "styled-components";
+import styled from "styled-components";
 import AttachmentIcon from "svgs/icons/attachment.svg";
 import { responsiveSize } from "styles/responsiveSize";
-import { landscapeStyle } from "styles/landscapeStyle";
 import { getIpfsUrl } from "utils/getIpfsUrl";
 
 const StyledA = styled(Link)`
-  height: fit-content;
   display: flex;
   gap: ${responsiveSize(5, 6)};
-  ${landscapeStyle(
-    () => css`
-      > svg {
-        width: 16px;
-        fill: ${({ theme }) => theme.primaryBlue};
-      }
-    `
-  )}
+
+  > svg {
+    width: 16px;
+    fill: ${({ theme }) => theme.primaryBlue};
+  }
 `;
 
 interface IAttachedFile {
