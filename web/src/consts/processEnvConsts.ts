56f3da3c4b503d4f9e6f58d7274d748d1f9f359b
kemuru <102478601+kemuru@users.noreply.github.com>
2024-08-26 18:12:27 +0200

    fix: process env variables outside vite

---
+++ web/src/consts/processEnvConsts.ts
@@ -0,0 +1,24 @@
+import { arbitrum, arbitrumSepolia } from "viem/chains"
+import { version, gitCommitHash, gitCommitShortHash, gitBranch, gitTags, clean } from "../generatedGitInfo.json";
+
+export const ONE_BASIS_POINT = 10000n;
+
+export const IPFS_GATEWAY = process.env.REACT_APP_IPFS_GATEWAY || "https://cdn.kleros.link";
+
+export const GIT_BRANCH = gitBranch;
+export const GIT_TAGS = gitTags;
+export const GIT_HASH = gitCommitShortHash;
+export const GIT_DIRTY = clean ? "" : "-dirty";
+export const GIT_URL = `https://github.com/kleros/escrow-v2/tree/${gitCommitHash}/web`;
+export const RELEASE_VERSION = version;
+
+// https://www.w3.org/TR/2012/WD-html-markup-20120329/input.email.html#input.email.attrs.value.single
+// eslint-disable-next-line security/detect-unsafe-regex
+export const EMAIL_REGEX = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+export const TELEGRAM_REGEX = /^@\w{5,32}$/;
+export const ETH_ADDRESS_REGEX = /^0x[a-fA-F0-9]{40}$/;
+export const ETH_SIGNATURE_REGEX = /^0x[a-fA-F0-9]{130}$/;
+
+export const isProductionDeployment = () => process.env.REACT_APP_DEPLOYMENT === "mainnet";
+
+export const DEFAULT_CHAIN = isProductionDeployment() ? arbitrum.id : arbitrumSepolia.id;
