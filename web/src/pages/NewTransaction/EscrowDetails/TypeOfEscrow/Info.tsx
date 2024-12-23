2f8962db4160f1adf31ad4cd9f9629f49bab4ad8
kemuru <102478601+kemuru@users.noreply.github.com>
2024-12-23 21:53:30 +0100

    chore: update logo, info text

--- web/src/pages/NewTransaction/EscrowDetails/TypeOfEscrow/Info.tsx
+++ web/src/pages/NewTransaction/EscrowDetails/TypeOfEscrow/Info.tsx
@@ -26,9 +26,9 @@ const Info: React.FC = () => {
         title={escrowType === "general" ? "General Escrow" : "Crypto Swap"}
         msg={
           escrowType === "general"
-            ? "Hiring an outside contractor? Use the General Escrow to safeguard your " +
-              "transactions. Use this option if you want to define the agreement " +
-              "under your own terms."
+            ? "Hiring an outside contractor? Making a P2P or OTC trade? Use the " +
+              "General Escrow to safeguard your transactions. Define the agreement " +
+              "under your own terms. Protected by Kleros Court to ensure a fair trade."
             : "Want to protect your crypto transaction? Use this option to create a " +
               "safe cross-chain swap. One person escrows an asset based on " +
               "Ethereum and the funds are released once assets on another " +
