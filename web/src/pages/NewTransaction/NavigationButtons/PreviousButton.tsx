01d2991d260e3e03cc622251298cda1b09dc07b1
kyrers <kyrers.eth@gmail.com>
2025-12-18 18:33:29 +0000

    refactor: migrate NewTransaction TypeOfEscrow and related components

--- web/src/pages/NewTransaction/NavigationButtons/PreviousButton.tsx
+++ web/src/pages/NewTransaction/NavigationButtons/PreviousButton.tsx
@@ -1,13 +1,7 @@
 import React from "react";
-import styled from "styled-components";
 import { Button } from "@kleros/ui-components-library";
 import { useNavigate } from "react-router-dom";
 import { isEmpty } from "src/utils";
-
-const StyledButton = styled(Button)<{ prevRoute: string }>`
-  display: ${({ prevRoute }) => (isEmpty(prevRoute) ? "none" : "flex")};
-`;
-
 interface IReturnButton {
   prevRoute: string;
 }
@@ -16,12 +10,12 @@ const ReturnButton: React.FC<IReturnButton> = ({ prevRoute }) => {
   const navigate = useNavigate();
 
   return (
-    <StyledButton
-      prevRoute={prevRoute}
-      onClick={() => navigate(prevRoute)}
+    <Button
+      className={isEmpty(prevRoute) ? "hidden" : "flex"}
+      onPress={() => navigate(prevRoute)}
       text="Return"
       variant="secondary"
-    ></StyledButton>
+    />
   );
 };
 
