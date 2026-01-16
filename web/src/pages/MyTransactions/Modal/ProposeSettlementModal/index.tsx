29aa19223687cd9161cbef8e441dcd12649ab940
kyrers <kyrers.eth@gmail.com>
2026-01-16 13:10:21 +0000

    refactor: migrate Modal in ProposeSettlementModal component

--- web/src/pages/MyTransactions/Modal/ProposeSettlementModal/index.tsx
+++ web/src/pages/MyTransactions/Modal/ProposeSettlementModal/index.tsx
@@ -1,30 +1,26 @@
-import React, { useRef, useState } from "react";
-import { useClickAway } from "react-use";
-import { Overlay } from "components/Overlay";
+import React, { useState } from "react";
 import AmountField from "./AmountField";
 import Buttons from "./Buttons";
-import StyledModal from "../StyledModal";
+import { Modal } from "@kleros/ui-components-library";
+import { baseModalStyle } from "~src/styles/modalStyles";
 
 interface IProposeSettlementModal {
+  isOpen: boolean;
   toggleModal: () => void;
   text: string;
 }
 
-const ProposeSettlementModal: React.FC<IProposeSettlementModal> = ({ toggleModal, text }) => {
-  const containerRef = useRef(null);
-  useClickAway(containerRef, () => toggleModal());
+const ProposeSettlementModal: React.FC<IProposeSettlementModal> = ({ isOpen, toggleModal, text }) => {
   const [amountProposed, setAmountProposed] = useState("0");
   const [isAmountValid, setIsAmountValid] = useState(true);
 
   return (
-    <Overlay>
-      <StyledModal ref={containerRef}>
-        <h1 className="m-0">{text}</h1>
-        <p className="m-0 mb-8 text-klerosUIComponentsSecondaryText">How much should be paid?</p>
-        <AmountField {...{ amountProposed, setAmountProposed, setIsAmountValid }} />
-        <Buttons {...{ toggleModal, amountProposed, isAmountValid }} />
-      </StyledModal>
-    </Overlay>
+    <Modal className={baseModalStyle} isOpen={isOpen} isDismissable onOpenChange={toggleModal}>
+      <h1 className="m-0">{text}</h1>
+      <p className="m-0 mb-8 text-klerosUIComponentsSecondaryText">How much should be paid?</p>
+      <AmountField {...{ amountProposed, setAmountProposed, setIsAmountValid }} />
+      <Buttons {...{ toggleModal, amountProposed, isAmountValid }} />
+    </Modal>
   );
 };
 
