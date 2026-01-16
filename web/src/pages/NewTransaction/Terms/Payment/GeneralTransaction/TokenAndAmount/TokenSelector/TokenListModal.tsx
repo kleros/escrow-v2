4c6f429bcf3ddef204a6b54b52f49949f85af2a1
kyrers <kyrers.eth@gmail.com>
2026-01-16 11:43:59 +0000

    refactor: migrate Modal in TokenListModal component

--- web/src/pages/NewTransaction/Terms/Payment/GeneralTransaction/TokenAndAmount/TokenSelector/TokenListModal.tsx
+++ web/src/pages/NewTransaction/Terms/Payment/GeneralTransaction/TokenAndAmount/TokenSelector/TokenListModal.tsx
@@ -1,41 +1,41 @@
-import React, { useRef, useState } from "react";
-import { useClickAway } from "react-use";
-import { Searchbar } from "@kleros/ui-components-library";
+import React, { useState } from "react";
+import { Modal, Searchbar } from "@kleros/ui-components-library";
 import { useNewTransactionContext } from "context/NewTransactionContext";
-import { Overlay } from "components/Overlay";
 import TokenItem from "./TokenItem";
-import StyledModal from "pages/MyTransactions/Modal/StyledModal";
 import { useFilteredTokens } from "hooks/useFilteredTokens";
+import clsx from "clsx";
+import { baseModalStyle } from "~src/styles/modalStyles";
 
-export const TokenListModal = ({ setIsOpen, tokens, setTokens, handleSelectToken }) => {
+export const TokenListModal = ({ isOpen, setIsOpen, tokens, setTokens, handleSelectToken }) => {
   const [searchQuery, setSearchQuery] = useState("");
   const { sendingToken } = useNewTransactionContext();
   const { filteredTokens } = useFilteredTokens(searchQuery, tokens, setTokens, sendingToken);
-  const containerRef = useRef(null);
-  useClickAway(containerRef, () => setIsOpen(false));
 
   return (
-    <Overlay>
-      <StyledModal className="w-fluid-320-500 lg:w-[500px]" ref={containerRef}>
-        <p className="flex self-start font-semibold m-0 mb-7">Select a token</p>
-        <Searchbar
-          className="w-full"
-          aria-label="Search by name or paste address"
-          placeholder="Search by name or paste address"
-          value={searchQuery}
-          onChange={(value) => setSearchQuery(value)}
-        />
-        <div className="flex flex-col w-full mt-6">
-          {filteredTokens.map((token) => (
-            <TokenItem
-              key={token.address}
-              selected={sendingToken?.address === token.address}
-              onSelect={handleSelectToken}
-              {...{ token }}
-            />
-          ))}
-        </div>
-      </StyledModal>
-    </Overlay>
+    <Modal
+      className={clsx(baseModalStyle, "w-fluid-320-500 lg:w-[500px]")}
+      isOpen={isOpen}
+      isDismissable
+      onOpenChange={setIsOpen}
+    >
+      <p className="flex self-start font-semibold m-0 mb-7">Select a token</p>
+      <Searchbar
+        className="w-full"
+        aria-label="Search by name or paste address"
+        placeholder="Search by name or paste address"
+        value={searchQuery}
+        onChange={(value) => setSearchQuery(value)}
+      />
+      <div className="flex flex-col w-full mt-6">
+        {filteredTokens.map((token) => (
+          <TokenItem
+            key={token.address}
+            selected={sendingToken?.address === token.address}
+            onSelect={handleSelectToken}
+            {...{ token }}
+          />
+        ))}
+      </div>
+    </Modal>
   );
 };
